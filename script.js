8607171cc80209c7386db5b2d2b9b6105a527ef9
M4TT4 <okamimatta@yahoo.com>
2025-05-06 14:57:17 -0500

    123123123

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ document.getElementById('dark-mode-toggle').addEventListener('click', () => {
     document.getElementById(id).classList.add('hidden');
   }
   
+
+  function openModal(id) {
+    const modal = document.getElementById(id);
+    modal.classList.remove('hidden');
+    const content = modal.querySelector('.modal-content');
+    content.style.top = `${100 + Math.random() * 200}px`;
+    content.style.left = `${100 + Math.random() * 200}px`;
+  }
